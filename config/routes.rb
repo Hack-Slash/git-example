cb4044671c6c4658f76b6da2dd6e9d32e8c66d4d
Marco <mgenova03@gmail.com>
2017-05-02 18:36:43 -0400

    add food index

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  get 'foods' => 'foods#index'
+
   get '/pages' => 'pages#index'
+  get '/pages/:id' => 'pages#show'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
