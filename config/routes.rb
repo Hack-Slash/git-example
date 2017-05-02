e7e48c860a7776589d47da9268e54aed5c9e1afa
Jamie MacKillop <jamieronin@gmail.com>
2017-05-02 18:38:50 -0400

    Add kittens controller and index action/route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   get '/pages' => 'pages#index'
-
+  get '/kittens' => 'kittens#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
